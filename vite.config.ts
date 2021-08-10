c2cdb3c48805fef380b9541a8f29a60e3b9ef7ad
Dang Van Thanh <dangvanthanh@dangthanh.org>
2021-08-10 23:21:16 +0700

    Update resolve path alias with typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { resolve } from 'path'
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import alias from '@rollup/plugin-alias'
+import resolve from '@rollup/plugin-node-resolve'
+
+const rootDir = path.resolve(__dirname)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     alias({
-      entries: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+      entries: [
+        {
+          find: '@',
+          replacement: path.resolve(rootDir, 'src'),
+        },
+      ],
     }),
+    resolve(),
   ],
 })
